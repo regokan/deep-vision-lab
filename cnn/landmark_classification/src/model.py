8296a82f9125bd5307830b8f8190b4b6ff061027
regokan <regokan@gmail.com>
2024-11-03 08:00:23 +0530

    add another model with higher dropout after each convolution layer

--- cnn/landmark_classification/src/model.py
+++ cnn/landmark_classification/src/model.py
@@ -61,3 +61,58 @@ class MyModel(nn.Module):
         x = self.conv_layers(x)  # Apply convolutional layers with BatchNorm and Dropout
         x = self.fc_layers(x)  # Apply fully connected layers
         return x
+
+
+class MyEnhancedModel(nn.Module):
+    def __init__(self, num_classes: int = 50, dropout: float = 0.3):
+        super().__init__()
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(
+                3, 32, kernel_size=3, stride=1, padding=1
+            ),  # Output: (32, 224, 224)
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # Output: (32, 112, 112)
+            nn.Dropout(p=dropout),
+            nn.Conv2d(
+                32, 64, kernel_size=3, stride=1, padding=1
+            ),  # Output: (64, 112, 112)
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # Output: (64, 56, 56)
+            nn.Dropout(p=dropout),
+            nn.Conv2d(
+                64, 128, kernel_size=3, stride=1, padding=1
+            ),  # Output: (128, 56, 56)
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # Output: (128, 28, 28)
+            nn.Dropout(p=dropout),
+            nn.Conv2d(
+                128, 256, kernel_size=3, stride=1, padding=1
+            ),  # Output: (256, 28, 28)
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # Output: (256, 14, 14)
+            nn.Dropout(p=dropout),
+            nn.Conv2d(
+                256, 512, kernel_size=3, stride=1, padding=1
+            ),  # Output: (512, 14, 14)
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # Output: (512, 7, 7)
+        )
+
+        self.fc_layers = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(512 * 7 * 7, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            nn.Linear(1024, num_classes),  # Output: (batch_size, 50)
+        )
+
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = self.fc_layers(x)
+        return x
